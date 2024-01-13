5a558a020f6979ed2e077478a330995e6c70f311
Steven Hester <45438379+stevenbhester@users.noreply.github.com>
2024-01-12 20:21:00 -0800

    Update beta.js

--- js/beta.js
+++ js/beta.js
@@ -10,7 +10,7 @@ function growBranch(parent, numLevels, angle, delay) {
 
     var currLvl = 1;
     // If we haven't reached the last level, create two more branches
-    if (currLvl < numLevels {
+    if (currLvl < numLevels) {
         growBranch(branch, currLvl + 1, 20, delay + 2); // Create right branch at next level
         growBranch(branch, currLvl + 1, -20, delay + 2); // Create left branch at next level
     }
