8a03b1df41f39bdbbed87996e46e162e1cc66b25
Steven Hester <45438379+stevenbhester@users.noreply.github.com>
2024-01-12 20:26:24 -0800

    Update beta.js

--- js/beta.js
+++ js/beta.js
@@ -8,11 +8,13 @@ function growBranch(parent, numLevels, angle, delay) {
     branch.style.animation = `growBranch 2s ${delay}s forwards`; // Animation with delay
     parent.appendChild(branch); // Add the new branch to its parent
 
-    var currLvl = 1;
+    var currDelay = delay;
+    
     // If we haven't reached the last level, create two more branches
-    if (currLvl < numLevels) {
-        growBranch(branch, currLvl + 1, 20, delay + 2); // Create right branch at next level
-        growBranch(branch, currLvl + 1, -20, delay + 2); // Create left branch at next level
+    for( let currLvl = 2; currLvl < numLevels, currLvl++) {
+        delay += 2;
+        growBranch(branch, currLvl, 20, delay); // Create right branch at next level
+        growBranch(branch, currLvl, -20, delay); // Create left branch at next level
     }
 }
 
